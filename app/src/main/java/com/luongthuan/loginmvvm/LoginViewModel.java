6f00c145672a68e6248b65617bab0381cf8caa4f
luongthuan-lv <luongthuan2k@gmail.com>
2020-06-30 17:05:18 +0700

    mvvm

--- app/src/main/java/com/luongthuan/loginmvvm/LoginViewModel.java
+++ app/src/main/java/com/luongthuan/loginmvvm/LoginViewModel.java
@@ -47,7 +47,7 @@ public class LoginViewModel extends ViewModel {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                loginModel.getEmail();
+
             }
 
             @Override
@@ -66,7 +66,7 @@ public class LoginViewModel extends ViewModel {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                loginModel.getPassword();
+
             }
 
             @Override
